8cb0f81db9158f5e9f1c3ef21285270a93bef125
mi_repositorio <72619423+gonzalomfp@users.noreply.github.com>
2021-09-29 17:57:33 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let nombre = prompt('Ingresa tu nombre')
+
+
+/*
+
+let nombre = prompt ('Ingresa tu nombre')
+let nombreIngresado = Text (nombre)
+
+let apellido = prompt ('Ingresa tu apellido')
+let apellidoIngresado = Text(apellido) */
+
+console.log ('Bienvenido: ' + nombre);
+
+alert('Bienvenido' + ' ' + (nombre));
+
+
+/*
+
+if (nombre == "") {
+    alert("No ingresaste el nombre de usuario");
+}
+else {
+    alert('Bienvenido' + ' ' + (nombre));
+}
+
+*/
