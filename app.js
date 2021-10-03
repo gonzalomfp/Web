3596bab6716a7cd2dcf48bbb93bf0290bb9e7128
mi_repositorio <72619423+gonzalomfp@users.noreply.github.com>
2021-10-03 10:41:35 -0300

    !

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-let nombre = prompt('Ingresa tu nombre')
+/*let nombre = prompt('Ingresa tu nombre')
 
 
-/*
+
 
 let nombre = prompt ('Ingresa tu nombre')
 let nombreIngresado = Text (nombre)
 
 let apellido = prompt ('Ingresa tu apellido')
-let apellidoIngresado = Text(apellido) */
+let apellidoIngresado = Text(apellido) 
 
 console.log ('Bienvenido: ' + nombre);
 
-alert('Bienvenido' + ' ' + (nombre));
-
-
-
-
-if (nombre == "") {
-    alert("No ingresaste el nombre de usuario");
+if (nombre === "") {
+    alert("No ingresaste tu nombre");
 }
 else {
     alert('Bienvenido' + ' ' + (nombre));
+}
+*/
+
+
+let i = prompt ('Ingresar un dato');
+
+while (i != "ESC") {
+    
+    alert(`El usuario ingresó ${i}`);
+
+    i = prompt('Ingresar otro dato');
 }
