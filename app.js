392f598ff1199824979197c35ab4c3ca43703d93
mi_repositorio <72619423+gonzalomfp@users.noreply.github.com>
2021-10-10 19:23:43 -0300

    1

--- app.js
+++ app.js
@@ -35,23 +35,43 @@ for (let index = 0; index < 10; i++) {
     
 } */
 
-let ingresoDeUsuario;
-let sumado;
+// let ingresoDeUsuario;
+// let sumado;
 
-function ingresarCosto() {
-    //let valor = prompt ("Ingrese por favor su $$");
-    let valor = 10;
-    return valor 
+// function ingresarCosto() {
+//     //let valor = prompt ("Ingrese por favor su $$");
+//     let valor = 10;
+//     return valor 
 
-}
-ingresoDeUsuario=ingresarCosto();
+// }
+// ingresoDeUsuario=ingresarCosto();
 
-function sumarIva (resultado){
-    return resultado *1.21;
-}
+// function sumarIva (resultado){
+//     return resultado *1.21;
+// }
 
-sumado = sumarIva (ingresoDeUsuario);
+// sumado = sumarIva (ingresoDeUsuario);
 
-function mostrarIva(precioCalculado){
-    console.log("el precio sumado es de : " + precioCalculado);
-}
+// function mostrarIva(precioCalculado){
+//     console.log("el precio sumado es de : " + precioCalculado);
+// }
+
+
+// CLASE 3 - Ciclos/iteraciones
+
+// const numero = Number(prompt('ingrese su numero'))
+
+// for (let i = 0; i <= 10; i++)
+// {
+//     console.log (`${numero} X ${i} = ${numero*i}`)
+
+// }
+
+// alert( "Turno N " +i+ " Nombre: "+ingresarNombre);
+
+
+// for(let i = 0; i<+5; i++){
+//     let ingresarNombre = prompt("Ingresar Nombre") 
+//     console.log("Nombre ingresado " + ingresarNombre)
+//}
+    
