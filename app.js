ec541bbbf34402e5e09035e6589dc3cf82c951b8
Gonzalo Fernandez <gonzalo.fp@icloud.com>
2021-10-14 06:54:28 -0300

    !

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
-let nombre = prompt('Decime tu nombre')
+//let nombre = prompt('Ingresá tu nombre')
 
-alert(`Hola ${nombre}`)
+// alert(`Hola ${nombre}`)
 
+// for (let i = 0; index < 10; i++) {
+
+// }
+
+// while (entrada !="ESC") {
+//  alert("El usuario ingresó " + entrada);
+//  entrada = prompt("Ingresar otro dato")
+//}
+
+
+// switch (nombre) {
+//     case 'Gonzalo':
+//         alert('Hola Gonzalo')
+//         break;
+
+//     case 'Matias':
+//         alert('Hola Matias')
+//         break;
+
+//     default:
+//         alert('No te conozco')
+//         break;
+// }
+
+// if(nombre === "gonzalo") {
+//     alert('Hola Gonzalo')
+// } else if (nombre === 'Matias'){
+//     alert('Hola Matias')
+// } else {
+//     alert('No sé quien sos')
+// }
+
+
+// animal = 123
+
+// console.log(typeof animal);
+
+//boolean
+//string
+//number
+//null
+//undefined
+//BigInt
+//Symbol
+
+// Tipo de datos primitos: No poseen propiedades ni metodos y son inmutables
+
+const persona1 = new Object({
+        nombre: 'Andres'
+    }
+
+)
+const persona2 = {
+    nombre: 'Andres'
+}
+
+console.log(typeof persona1)
+console.log(typeof persona2)
+
+console.log(persona1);
+console.log(persona2);
+
+
+// 37:42
