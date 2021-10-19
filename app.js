18a8598c34e78b71fd0b419660767b69f97bc2c2
Gonzalo Fernandez <gonzalo.fp@icloud.com>
2021-10-19 08:32:31 -0300

    Desafio 5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//let nombre = prompt('Ingresá tu nombre')
+// let nombre = prompt('Ingresá tu nombre')
 
-// alert(`Hola ${nombre}`)
+// alert(`¡Hola ${nombre}! Bienvenido a mi Web`)
 
 // for (let i = 0; index < 10; i++) {
 
@@ -49,20 +49,53 @@
 
 // Tipo de datos primitos: No poseen propiedades ni metodos y son inmutables
 
-const persona1 = new Object({
-        nombre: 'Andres'
-    }
+// const persona1 = new Object({
+//         nombre: 'Andres'
+//     }
 
-)
-const persona2 = {
-    nombre: 'Andres'
+// )
+// const persona2 = {
+//     nombre: 'Andres',
+//     edad: 25,
+
+//     saludar: function() {
+//         console.log('Hola, que tal?')
+//     }
+// }
+
+// console.log(typeof persona1)
+// console.log(typeof persona2)
+
+// console.log(persona1)
+
+// console.log(persona2.nombre);
+
+// console.log(persona2['nombre']);
+
+// console.log(persona2.saludar());
+
+// persona2.profesion = 'Desarrollador'
+
+// console.log(persona2);
+
+
+function Persona(nombre, nacionalidad) {
+
+    this.nombre = nombre;
+    this.nacionalidad = nacionalidad;
+
 }
 
-console.log(typeof persona1)
-console.log(typeof persona2)
 
-console.log(persona1);
-console.log(persona2);
+let nombre = prompt('Ingresá tu nombre')
+let nacionalidad = prompt('Nacionalidad')
 
+const persona1 = new Persona(nombre, nacionalidad)
 
-// 37:42
+console.log(persona1)
+
+console.log(typeof persona1);
+
+alert(`¡Hola ${nombre}, gracias por seguirme desde ${nacionalidad}!`)
+
+//01:30
