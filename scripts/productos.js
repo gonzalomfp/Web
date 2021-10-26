15776fcc7613f38ce86f694431683414b49ee266
Gonzalo Fernandez <gonzalo.fp@icloud.com>
2021-10-26 00:37:44 -0300

    + js de productos

--- scripts/productos.js
+++ scripts/productos.js
@@ -3,8 +3,31 @@ let productos = [
         nombre: "Buzo Mundo",
         color: ["Azul", "Negro", "Blanco", "Bordo"],
         talle: ["xs", "s", "m", "l", "xl"],
-        precio: 3600
+        precio: 6500
+    },
+    {
+        nombre: "Buzo Viaje",
+        color: ["Negro", "Crema"],
+        talle: ["s", "m", "l"],
+        precio: 6500
+    },
+    {
+        nombre: "Remera Viaje",
+        color: ["Negra"],
+        talle: ["s", "m", "l", "xl"],
+        precio: 2400
+    },
+    {
+        nombre: "Remera Viaje 2",
+        color: ["Blanca"],
+        talle: ["s", "m", "l", "xl"],
+        precio: 2400
+    },
+    {
+        nombre: "Barbijo ViajeroSecrets",
+        color: "Unico",
+        talle: "Unico",
+        precio: 690
     }
-
 
 ]
