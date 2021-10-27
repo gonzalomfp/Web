fb4b273c9ff949d824f9076d15535ae96be95034
Gonzalo Fernandez <gonzalo.fp@icloud.com>
2021-10-27 10:42:42 -0300

    Estilo Html

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,9 +1,9 @@
 let productos = [
     {
-        nombre: "Buzo Mundo",
+        nombre: "Buzo ViajeroSecrets",
         color: ["Azul", "Negro", "Blanco", "Bordo"],
         talle: ["xs", "s", "m", "l", "xl"],
-        precio: 6500
+        precio: 7000
     },
     {
         nombre: "Buzo Viaje",
@@ -12,14 +12,26 @@ let productos = [
         precio: 6500
     },
     {
-        nombre: "Remera Viaje",
-        color: ["Negra"],
+        nombre: "Buzo Londres",
+        color: "Negro",
+        talle: ["s", "m", "l"],
+        precio: 6500
+    },
+    {
+        nombre: "Remera Emojis",
+        color: "Negro",
         talle: ["s", "m", "l", "xl"],
         precio: 2400
     },
     {
-        nombre: "Remera Viaje 2",
-        color: ["Blanca"],
+        nombre: "Remera Viaje",
+        color: "Gris",
+        talle: ["s", "m", "l", "xl"],
+        precio: 2400
+    },
+    {
+        nombre: "Remera Destino",
+        color: "Blanco",
         talle: ["s", "m", "l", "xl"],
         precio: 2400
     },
@@ -28,6 +40,18 @@ let productos = [
         color: "Unico",
         talle: "Unico",
         precio: 690
+    },
+    {
+        nombre: "Barbijo Mundi",
+        color: "Unico",
+        talle: "Unico",
+        precio: 650
+    },
+    {
+        nombre: "Barbijo Sellos",
+        color: "Unico",
+        talle: "Unico",
+        precio: 650
     }
 
 ]
